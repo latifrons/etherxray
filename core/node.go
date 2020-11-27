4308343be8f842a784d0ccd9a55a26bdcd1fa766
latifrons <latifrons88@gmail.com>
2020-11-27 23:27:43 +0800

    Init

---
+++ core/node.go
@@ -0,0 +1,35 @@
+package core
+
+import (
+	"github.com/sirupsen/logrus"
+)
+
+type Node struct {
+	DataFolder string
+
+	components []Component
+}
+
+func (n *Node) Setup() {
+}
+
+func (n *Node) Start() {
+	for _, component := range n.components {
+		logrus.Infof("Starting %s", component.Name())
+		component.Start()
+		logrus.Infof("Started: %s", component.Name())
+
+	}
+	//n.heightEventChan <- 10943851
+	logrus.Info("Node Started")
+}
+
+func (n *Node) Stop() {
+	for i := len(n.components) - 1; i >= 0; i-- {
+		comp := n.components[i]
+		logrus.Infof("Stopping %s", comp.Name())
+		comp.Stop()
+		logrus.Infof("Stopped: %s", comp.Name())
+	}
+	logrus.Info("Node Stopped")
+}
